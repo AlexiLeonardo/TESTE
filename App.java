20b6137a8483552eb3192e87b9cbfe089df67ac3
AlexiLeonardo <118396436+AlexiLeonardo@users.noreply.github.com>
2023-09-09 15:45:47 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("\nHi!");
-        String version = System.getProperty("java.version");
-        System.out.println("Running Java Version "+version+"\n");
+    public static void main(String[] args){
+       
+      
     }
 }
